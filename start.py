a1e415826b4f688aa9c5e225e9945eb92289eb2b
igc5972 <41637773+igc5972@users.noreply.github.com>
2022-06-22 16:13:48 -0400

    Initial push

---
+++ start.py
@@ -0,0 +1,74 @@
+##################################################################################
+### IMPORT STATEMENTS
+##################################################################################
+import numpy as np
+from matplotlib import pyplot as plt
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from astropy.io import fits
+from astroquery.sdss import SDSS
+
+
+
+##################################################################################
+### TEST OBJECT
+##################################################################################
+
+#a completely random star I selected
+ra =  10.187604
+dec = 0.21871262
+
+
+#coordinates of star to use for testing
+test_crd = SkyCoord(ra=ra*u.deg, dec=dec*u.deg)
+
+
+
+
+##################################################################################
+### FUNCTION: OBTAIN SPECTRUM OF OBJECT FROM SDSS
+##################################################################################
+def get_spectrum(crd):
+    '''
+    Input:    An astropy SkyCoord object for the desired target
+    Returns:  The wavelength and flux of the SDSS spectrum
+    '''
+
+    sp = SDSS.get_spectra(coordinates = crd, radius=2*u.arcsec)[0][1]
+
+    sp_data = sp.data
+
+    sp_flux = sp_data.flux
+    sp_lambda = 10**sp_data.loglam #lambda is returned in log space
+
+    return(sp_lambda, sp_flux)
+
+
+
+
+##################################################################################
+### FUNCTION: PLOT THE SPECTRUM
+##################################################################################
+
+def plot_spectrum(wave, flux):
+    '''
+    Inputs:  Two lists, one for the wavelength and one for the fluxes in the spectrum
+    Returns: Nothing, outputs a plot for visualization
+    '''
+    fig, ax = plt.subplots(figsize = (10, 5))
+    ax.plot(wave, flux, lw = 2, color = 'k')
+    ax.set_xlabel(r'$\rm{Wavelength \ [\AA]}$', fontsize = 14)
+    ax.set_ylabel(r'$\mathrm{Flux \ [arbitrary units]}$', fontsize = 14)
+    plt.show()
+
+
+
+##################################################################################
+### FUNCTION: TEST THE FUNCTIONS
+##################################################################################
+
+wave, flux = get_spectrum(test_crd)
+plot_spectrum(wave, flux)
+
+
+
