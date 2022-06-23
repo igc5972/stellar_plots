def3a14e824be591d94c41ae76e3ab56a40b58ba
igc5972 <41637773+igc5972@users.noreply.github.com>
2022-06-23 17:23:46 -0400

    finalized final function

--- stellar_plots/stellar_plots.py
+++ stellar_plots/stellar_plots.py
@@ -1,77 +1,46 @@
 ##################################################################################
-### IMPORT STATEMENTS
-##################################################################################
-import numpy as np
-from matplotlib import pyplot as plt
-from astropy.coordinates import SkyCoord
-from astropy import units as u
-from astropy.io import fits
-from astroquery.sdss import SDSS
-
-
-
-##################################################################################
-### TEST OBJECT
+### PURPOSE: Main calling function
 ##################################################################################
 
-#a completely random star I selected
-ra =  1087604
-dec = 0.71262
-
-
-#coordinates of star to use for testing
-test_crd = SkyCoord(ra=ra*u.deg, dec=dec*u.deg)
-
-
-
 ##################################################################################
-### FUNCTION: OBTAIN SPECTRUM OF OBJECT FROM SDSS
+### PURPOSE: Import Statements
 ##################################################################################
-def get_spectrum(crd):
-    '''
-    Input:    An astropy SkyCoord object for the desired target
-    Returns:  The wavelength and flux of the SDSS spectrum
+from matplotlib.backends.backend_pdf import PdfPages
+from make_plot.py import plotting
+from utils.py import *
+
+
+
+def output_figures(input_file, output_save_dir_plus_name):
+
+    '''Main calling function
+
+    Main function that calls other functions to find properties for the list of sources
+
+    Args:
+        input_file (str): Points to a list (.txt) with two columns (ra, dec) and 
+            1 row per source with 1 header row
+        output_save_dir_plus_name(str): The path and name for where the flipbook
+            should be saved on user's computer
+
+    Returns:
+        None
     '''
 
-    #Catch TypeErrors for objects that do not exist in SDSS
-    try:
-        sp = SDSS.get_spectra(coordinates = crd, radius=2*u.arcsec)[0][1]
+    
+    pp = PdfPages(save_dir_plus_name) #directory to save output flipbook to
 
-        sp_data = sp.data
+    ras, decs = np.loadtxt(list, unpack = True, skiprows = 1)
+    temp, gmag = {function call to Gaia}
+    wave, flux = {function call to SDSS}
+    ....
 
-        sp_flux = sp_data.flux
-        sp_lambda = 10**sp_data.loglam #lambda is returned in log space
+    for i in range(len(ras)):
+        figure = plotting(args...)
+        pp.savefig(figure)
 
-        return(sp_lambda, sp_flux)
-
-    except(ValueError, RuntimeError, TypeError):
-        return(-99, -99)
-
-
-
-
-##################################################################################
-### FUNCTION: PLOT THE SPECTRUM
-##################################################################################
-def plot_spectrum(wave, flux):
-    '''
-    Inputs:  Two lists, one for the wavelength and one for the fluxes in the spectrum
-    Returns: Nothing, outputs a plot for visualization
-    '''
-    fig, ax = plt.subplots(figsize = (10, 5))
-    ax.plot(wave, flux, lw = 2, color = 'k')
-    ax.set_xlabel(r'$\rm{Wavelength \ [\AA]}$', fontsize = 14)
-    ax.set_ylabel(r'$\mathrm{Flux \ [arbitrary units]}$', fontsize = 14)
-    plt.show()
-
-
-
-##################################################################################
-### FUNCTION: TEST THE FUNCTIONS
-##################################################################################
-
-wave, flux = get_spectrum(test_crd)
-plot_spectrum(wave, flux)
-
-
-
+    pp.close()
+        
+        
+        
+    
